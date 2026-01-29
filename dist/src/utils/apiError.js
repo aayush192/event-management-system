e74f353b51341039d05f7e82eac29046f53d0bd1
Aayush Khadka <ayushkhadka9847@gmail.com>
2026-01-29 20:10:39 +0545

    Normalize file casing in controller folder

--- dist/src/utils/apiError.js
+++
@@ -1,8 +0,0 @@
-export const errorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const status = err.status || "error";
-    res.status(statusCode).json({
-        status,
-        message: err.message || "Internal Server Error",
-    });
-};
